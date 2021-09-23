a105bf68944117bc16e3669555ebd2e5c836d664
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-09-23 11:03:47 +0530

    vote badge added

--- movie-hub/src/components/SingleContent/SingleContent.js
+++ movie-hub/src/components/SingleContent/SingleContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Badge from '@mui/material/Badge';
 import { img_300, unavailable } from "../../config/config";
 import "./SingleContent.css";
 
@@ -12,12 +13,17 @@ const SingleContent = ({
 }) => {
     return (
         <div className="media">
+               <Badge
+                    badgeContent={vote_average}
+                    color={vote_average > 6 ? "primary" : "error"}
+               />
                <img src={poster ? `${img_300}/${poster}` : unavailable} alt={title}  className="poster"/>
                <b className="title">{title}</b>
                <span className="subTitle">
                      {media_type === "tv" ? "TV Series" : "Movie"}
                       <span className="subTitle">{date}</span>
               </span>
+              
         </div>
     );
 };
